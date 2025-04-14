ee9ef417df8cecb228b8438a1d0ec34549415caf
Paul Osmialowski <pawel.osmialowski@arm.com>
2025-04-14 21:56:45 +0100

    [ATfL] Restore obligatory linking with -ldl. Apparently this is needed for RHEL8 (#281)
    It's a cherry-pick from the arm-software branch.
    
    In my previous commit I've misread the reason why those two lines were
    introduced. Now we cannot build for RHEL8 and this patch restores those
    two lines.

--- arm-software/linux/build.sh
+++ arm-software/linux/build.sh
@@ -382,6 +382,8 @@ shared_lib_build() {
     cp -d ${ATFL_DIR}.libs/lib/libFortranRuntime.so* \
         "${ATFL_DIR}/lib/${ATFL_TARGET_TRIPLE}"
     rm -r "${ATFL_DIR}.libs"
+    echo '-L<CFGDIR>/../runtimes/runtimes-bins/openmp/runtime/src $-Wl,--push-state $-Wl,--as-needed $-lomp $-ldl $-Wl,--pop-state' >bin/clang.cfg
+    echo '-L<CFGDIR>/../runtimes/runtimes-bins/openmp/runtime/src $-Wl,--push-state $-Wl,--as-needed $-lomp $-ldl $-Wl,--pop-state' >bin/clang++.cfg
     run_command ninja ${NINJA_ARGS} check-all | tee -a "${LOGS_DIR}/shared_lib.txt"
 }
 
