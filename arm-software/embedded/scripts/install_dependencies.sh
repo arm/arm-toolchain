592c3ffa19cc46a18b91b35c70fa310c106ade35
Hugo Silva <hugo.silva@arm.com>
2025-04-15 10:05:34 +0100

    Specify dependency versions to install

--- arm-software/embedded/scripts/install_dependencies.sh
+++ arm-software/embedded/scripts/install_dependencies.sh
@@ -4,12 +4,18 @@
 # Part of the Arm Toolchain project, under the Apache License v2.0 with LLVM Exceptions.
 # See https://llvm.org/LICENSE.txt for license information.
 # SPDX-License-Identifier: Apache-2.0 WITH LLVM-exception
+#
+# This script installs the essential build dependencies for ATfE.
 
 set -e
 
-# This script installs the essential build dependencies for ATfE.
-
 sudo apt-get update && sudo apt-get install -y --no-install-recommends \
-    cmake \
-    ninja-build \
-    clang
+    cmake=3.22.1-1ubuntu1.22.04.2 \
+    ninja-build=1.10.1-1 \
+    clang=1:14.0-55~exp2 \
+    python3-pip \
+    python3-setuptools
+
+# Upgrade pip and install meson with a pinned version
+python3 -m pip install --upgrade pip
+python3 -m pip install meson==1.2.3
