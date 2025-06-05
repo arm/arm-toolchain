9e8a0f6c51edc8174a80a016282260ec8adfd77a
Vrukesh V Panse <vrukesh.panse@arm.com>
2025-06-05 13:11:54 +0100

    Enable address sanitizer in Github Action for ATfE.

---
+++ arm-software/embedded/scripts/test_atfe_sanitizer.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+# Copyright (c) 2025, Arm Limited and affiliates.
+# Part of the Arm Toolchain project, under the Apache License v2.0 with LLVM Exceptions.
+# See https://llvm.org/LICENSE.txt for license information.
+# SPDX-License-Identifier: Apache-2.0 WITH LLVM-exception
+
+# A bash script to run the tests from the Arm Toolchain for Embedded.
+
+# The script assumes a successful build of the toolchain exists in the 'build'
+# directory inside the repository tree.
+
+set -ex
+
+SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
+REPO_ROOT=$( git -C "${SCRIPT_DIR}" rev-parse --show-toplevel )
+
+cd "${REPO_ROOT}"/build
+
+# The llvm-toolchain targets already set --xunit-xml-output so
+# only the --ignore-fail option is needed.
+# The picolibc tests do not use lit so do not support this option.
+export LIT_OPTS="--ignore-fail"
+ninja check-llvm-toolchain check-cxxabi check-unwind check-package-llvm-toolchain
