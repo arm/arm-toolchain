540c5e8243228e195b8c3d4ced33c471e65979d8
Vrukesh V Panse <vrukesh.panse@arm.com>
2025-06-19 10:58:22 +0100

    Update test script for sanitizers, to handle the failure in check-all tests.

--- arm-software/embedded/scripts/test_atfe_sanitizer.sh
+++ arm-software/embedded/scripts/test_atfe_sanitizer.sh
@@ -21,12 +21,22 @@ PROCESSOR_COUNT=$(getconf _NPROCESSORS_ONLN)
 
 cd "${REPO_ROOT}"/build
 
+# If a test fails, lit will ordinarily return a non-zero result,
+# which prevents further testing. Setting the --ignore-fail option
+# will cause testing to continue, so that CI systems can get a
+# full set of results.
+# The check-all target runs the upstream clang and LLVM tests,
+# which do not generate an junit xml results file by default.
+# Additionally setting the --xunit-xml-output option store the
+# results.
+export LIT_OPTS="--ignore-fail --xunit-xml-output=results.xml"
+ninja -j$PROCESSOR_COUNT check-all
+
 # The llvm-toolchain targets already set --xunit-xml-output so
 # only the --ignore-fail option is needed.
 # The picolibc tests do not use lit so do not support this option.
 export LIT_OPTS="--ignore-fail"
-ninja -j$PROCESSOR_COUNT check-all \
-    check-compiler-rt-armv7m_hard_fpv5_d16_exn_rtti_unaligned \
+ninja -j$PROCESSOR_COUNT check-compiler-rt-armv7m_hard_fpv5_d16_exn_rtti_unaligned \
     check-picolibc-armv7m_hard_fpv5_d16_exn_rtti_unaligned \
     check-cxx-armv7m_hard_fpv5_d16_exn_rtti_unaligned \
     check-cxxabi-armv7m_hard_fpv5_d16_exn_rtti_unaligned \
