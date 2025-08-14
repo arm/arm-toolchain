8888630721a8dd6a1854553a743c1ac65ed1e78f
William Huynh <William.Huynh@arm.com>
2025-08-14 14:43:21 +0100

    [ATfE] Enable v7-A and v7-R variants for LLVM-libc tests
    Support added in https://github.com/llvm/llvm-project/pull/153576

--- arm-software/embedded/llvmlibc-support/crt0/exceptions_7a.h
+++ arm-software/embedded/llvmlibc-support/crt0/exceptions_7a.h
@@ -95,7 +95,9 @@ EXFN_ATTR void handle_fiq() {
 // bytes long, and contains code. The whole table must be 32-byte aligned.
 // The table may also be relocated, so we make it position-independent by
 // having a table of handler addresses and loading the address to pc.
-__attribute__((naked, section(".vectors"), aligned(32))) void vector_table() {
+[[gnu::section(".vectors"), gnu::aligned(32), gnu::used, gnu::naked,
+  gnu::target("arm")]]
+void vector_table() {
   asm("LDR pc, [pc, #24]");
   asm("LDR pc, [pc, #24]");
   asm("LDR pc, [pc, #24]");
@@ -104,14 +106,14 @@ __attribute__((naked, section(".vectors"), aligned(32))) void vector_table() {
   asm("LDR pc, [pc, #24]");
   asm("LDR pc, [pc, #24]");
   asm("LDR pc, [pc, #24]");
-  asm(".word %0" : : "X"(handle_reset));
-  asm(".word %0" : : "X"(handle_undefined));
-  asm(".word %0" : : "X"(handle_svc_hyp_smc));
-  asm(".word %0" : : "X"(handle_prefetch_abort));
-  asm(".word %0" : : "X"(handle_data_abort));
-  asm(".word %0" : : "X"(handle_hyp_trap));
-  asm(".word %0" : : "X"(handle_irq));
-  asm(".word %0" : : "X"(handle_fiq));
+  asm(".word %c0" : : "X"(handle_reset));
+  asm(".word %c0" : : "X"(handle_undefined));
+  asm(".word %c0" : : "X"(handle_svc_hyp_smc));
+  asm(".word %c0" : : "X"(handle_prefetch_abort));
+  asm(".word %c0" : : "X"(handle_data_abort));
+  asm(".word %c0" : : "X"(handle_hyp_trap));
+  asm(".word %c0" : : "X"(handle_irq));
+  asm(".word %c0" : : "X"(handle_fiq));
 }
 
 } // namespace exceptions
