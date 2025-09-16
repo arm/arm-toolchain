232cd31ca786c3228fb0dba8bc0fa85caee08080
David Candler <david.candler@arm.com>
2025-09-16 11:37:39 +0100

    [ATfE] Use a consistent name for xml results
    Currently, some test suites are hard-coded to use a specific
    name/path to write xml results to. Others rely on this being set
    manually. Some of the CI scripts don't set it at all for certain
    test targets.
    
    This patch aligns all the test suites to use the same name,
    controlled via the same mechanism. A user or CI system can set
    the `--xunit-xml-output` lit option via the `LIT_OPTS` environment
    variable to control the name of xml file, and therefore know the
    correct name to look for when storing the results.
    
    A few minor corrections have also been made to the CI scripts
    for running tests:
    
    * Since ninja defaults to bulding in parallel, so there is no reason
    to set the `-j` option.
    * `check-all` was getting bult twice in some scripts.
    * Comments and lit options updated to reflect that picolibc
    does in fact use lit now.

--- arm-software/embedded/arm-runtimes/test-support/modify-compiler-rt-xml.py
+++ arm-software/embedded/arm-runtimes/test-support/modify-compiler-rt-xml.py
@@ -8,6 +8,7 @@
 
 import argparse
 import os
+import re
 from xml.etree import ElementTree
 
 
@@ -25,7 +26,26 @@ def main():
     )
     args = parser.parse_args()
 
-    xml_file = os.path.join(args.dir, "compiler-rt", "test", "results.junit.xml")
+    xml_file = None
+    # The xml file path can be set by lit's --xunit-xml-output option.
+    # Since ATfE does not set this directly, it will likely be found
+    # in the LIT_OPTS environment variable, which lit will read
+    # options from.
+    if "LIT_OPTS" in os.environ:
+        lit_opts = os.environ["LIT_OPTS"]
+        m = re.search("--xunit-xml-output=([^ ]+)", lit_opts)
+        if m is not None:
+            results_path = m.group(1)
+            # Path may be absolute or relative.
+            if os.path.isabs(results_path):
+                xml_file = results_path
+            else:
+                # If not absolute, the path will be relative to compiler-rt/test
+                # in the build directory, not this script.
+                xml_file = os.path.join(args.dir, "compiler-rt", "test", results_path)
+    if xml_file is None:
+        print(f"No xml results generated to modify.")
+        return
 
     tree = ElementTree.parse(xml_file)
     root = tree.getroot()
--- arm-software/embedded/scripts/test.sh
+++ arm-software/embedded/scripts/test.sh
@@ -15,21 +15,20 @@ set -ex
 SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
 REPO_ROOT=$( git -C "${SCRIPT_DIR}" rev-parse --show-toplevel )
 
-cd "${REPO_ROOT}"/build
-
 # If a test fails, lit will ordinarily return a non-zero result,
 # which prevents further testing. Setting the --ignore-fail option
 # will cause testing to continue, so that CI systems can get a
 # full set of results.
-# The check-all target runs the upstream clang and LLVM tests,
-# which do not generate an junit xml results file by default.
-# Additionally setting the --xunit-xml-output option store the
-# results.
-export LIT_OPTS="--ignore-fail --xunit-xml-output=results.xml"
-ninja check-all
+# The lit test suites do not generate xml results by default.
+# This can be enabled with the --xunit-xml-output option. The file
+# written will be relative to the individual suite's build directly,
+# so the same name can be used for all files for consistency.
+export LIT_OPTS="--ignore-fail --xunit-xml-output=lit_results.junit.xml"
 
-# The llvm-toolchain targets already set --xunit-xml-output so
-# only the --ignore-fail option is needed.
-# The picolibc tests do not use lit so do not support this option.
-export LIT_OPTS="--ignore-fail"
-ninja check-llvm-toolchain check-cxxabi check-unwind check-package-llvm-toolchain
+# Run all relevant test targets using Ninja.
+cd "${REPO_ROOT}"/build
+ninja check-all
+ninja check-llvm-toolchain
+ninja check-cxxabi
+ninja check-unwind
+ninja check-package-llvm-toolchain
