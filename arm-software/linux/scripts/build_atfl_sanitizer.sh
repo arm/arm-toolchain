31cb2c58e84b384952eee230b9a1b2e82dd5bcfa
Vrukesh Panse <vrukesh.panse@gmail.com>
2025-10-13 15:54:21 +0100

    Update build_atfl_sanitizer.sh

--- arm-software/linux/scripts/build_atfl_sanitizer.sh
+++ arm-software/linux/scripts/build_atfl_sanitizer.sh
@@ -68,16 +68,17 @@ export ASAN_OPTIONS=detect_leaks=0
 # We have explicitly disabled memory leak detection, since the observed issues are
 # unrelated to product under test.
 cmake -G Ninja ../llvm \
-    -DCMAKE_BUILD_TYPE=Release \
-    -DLLVM_USE_SANITIZER="Address;Undefined" \
-    -DLLVM_ENABLE_ASSERTIONS=True \
-    -DCMAKE_C_COMPILER="${REPO_ROOT}/build_llvm/bin/clang" \
-    -DCMAKE_CXX_COMPILER="${REPO_ROOT}/build_llvm/bin/clang++" \
-    -DLLVM_LIT_ARGS="-v --param=env='ASAN_OPTIONS=detect_leaks=0'" \
-    -DCMAKE_INSTALL_PREFIX=../stage1.install \
-    -DLLVM_TARGETS_TO_BUILD=AArch64 \
-    -DLLVM_ENABLE_PROJECTS="clang-tools-extra;clang;llvm" \
-    -DLLVM_ENABLE_RUNTIMES="libcxx;libcxxabi;libunwind;compiler-rt"
+  -DCMAKE_BUILD_TYPE=Release \
+  -DLLVM_USE_SANITIZER="Address;Undefined" \
+  -DLLVM_ENABLE_ASSERTIONS=True \
+  -DCMAKE_C_COMPILER="${REPO_ROOT}/build_llvm/bin/clang" \
+  -DCMAKE_CXX_COMPILER="${REPO_ROOT}/build_llvm/bin/clang++" \
+  -DLLVM_LIT_ARGS="-v --param=env='ASAN_OPTIONS=detect_leaks=0' \
+    --filter-out 'time.zone|tzdb|time.clock|orc|activation-options|quarantine_size|mmap_write_exec|log-path_test|assert.cpp|initialization-nobug|use_globals|closed-fds'" \
+  -DCMAKE_INSTALL_PREFIX=../stage1.install \
+  -DLLVM_TARGETS_TO_BUILD=AArch64 \
+  -DLLVM_ENABLE_PROJECTS="clang-tools-extra;clang;llvm" \
+  -DLLVM_ENABLE_RUNTIMES="libcxx;libcxxabi;libunwind;compiler-rt"
 
 ninja
 echo "==> Stage 3: Completed building clang (sanitized)"
