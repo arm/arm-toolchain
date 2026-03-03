70edb8b128057f77b8419d223d690febe660dca2
Hugo da Silva <hugo.silva@arm.com>
2026-03-03 14:45:05 +0000

    Build ATfE with picolibc and LLVM libc (#763)
    - Update build.sh and build_pre_merge.sh to explicitly build ATfE with
    both C libraries
    - Remove llvm libc overlay job and script
    
    (cherry picked from commit 4520ad2a2ec18a1b81f9a634c309df1457bd50cc)

--- arm-software/embedded/scripts/build_newlib_nano_overlay.sh
+++ arm-software/embedded/scripts/build_newlib_nano_overlay.sh
@@ -22,7 +22,10 @@ BUILD_DIR=${REPO_ROOT}/build_newlib_nano_overlay
 mkdir -p "${BUILD_DIR}"
 cd "${BUILD_DIR}"
 
-cmake ../arm-software/embedded -GNinja -DFETCHCONTENT_QUIET=OFF -DLLVM_TOOLCHAIN_C_LIBRARY=newlib-nano -DLLVM_TOOLCHAIN_LIBRARY_OVERLAY_INSTALL=on
+cmake ../arm-software/embedded -GNinja -DFETCHCONTENT_QUIET=OFF \
+    -DLLVM_TOOLCHAIN_ENABLE_PICOLIBC=OFF \
+    -DLLVM_TOOLCHAIN_ENABLE_NEWLIB_NANO=ON \
+    -DLLVM_TOOLCHAIN_LIBRARY_OVERLAY_INSTALL=on
 ninja package-llvm-toolchain
 
 # The package-llvm-toolchain target will produce a .tar.xz package, but we also
