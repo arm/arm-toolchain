5f89beeb5b633b2ac19be4c4a16356a0601ff65d
Vrukesh V Panse <vrukesh.panse@arm.com>
2026-04-08 11:20:44 +0100

    [ATfL]: Add Github action for nightly build and dependency script

--- arm-software/linux/scripts/install_dependencies_ubuntu.sh
+++ arm-software/linux/scripts/install_dependencies_ubuntu.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-# Copyright (c) 2025, Arm Limited and affiliates.
+# Copyright (c) 2026, Arm Limited and affiliates.
 # Part of the Arm Toolchain project, under the Apache License v2.0 with LLVM Exceptions.
 # See https://llvm.org/LICENSE.txt for license information.
 # SPDX-License-Identifier: Apache-2.0 WITH LLVM-exception
@@ -25,6 +25,7 @@ sudo apt-get update && sudo apt-get install -y --no-install-recommends \
     python3-pip \
     python3-pygments \
     python3-setuptools \
+    python3-sphinx \
     python3-yaml \
     zlib1g-dev \
     valgrind
