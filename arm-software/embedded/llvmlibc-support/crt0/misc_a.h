431977476aedd165d14acedacd678a3ff62446cc
Volodymyr Turanskyy <volodymyr.turanskyy@arm.com>
2026-05-01 16:46:57 +0100

    [ATfE] Implement __arm_sme_state in LLVM libc bootcode
    This adds LLVM libc bootcode support for __arm_sme_state as per
    https://github.com/ARM-software/abi-aa/blob/main/aapcs64/aapcs64.rst#811__arm_sme_state
    to match this LLVM change https://github.com/llvm/llvm-project/pull/191434

--- arm-software/embedded/llvmlibc-support/crt0/misc_a.h
+++ arm-software/embedded/llvmlibc-support/crt0/misc_a.h
@@ -18,6 +18,35 @@ namespace misc {
 
 using namespace sysreg;
 
+#if defined(__ARM_ARCH_ISA_A64)
+[[clang::always_inline]] inline bool has_sme_or_sme2() {
+  constexpr auto FEAT_SME_1 = 1UL << 24;
+  constexpr auto FEAT_SME_2 = 1UL << 25;
+
+  return (static_cast<unsigned long>(ID_AA64PFR1) & (FEAT_SME_1 | FEAT_SME_2)) != 0;
+}
+
+[[clang::always_inline]] inline void init_sme_state() {
+  if (!has_sme_or_sme2())
+    return;
+
+  // TPIDR2_EL0 resets to an architecturally unknown value, so clear it.
+  asm volatile("msr s3_3_c13_c0_5, xzr\n\tisb" : : : "memory");
+
+  // Try to set the streaming vector length to the architectural maximum.
+  unsigned long smcr;
+  if (__arm_rsr("CurrentEL") == 3 << 2) {
+    asm volatile("mrs %0, s3_6_c1_c2_6" : "=r"(smcr));
+    smcr = (smcr & ~0xfUL) | 0xfUL;
+    asm volatile("msr s3_6_c1_c2_6, %0\n\tisb" : : "r"(smcr) : "memory");
+  } else if (__arm_rsr("CurrentEL") == 2 << 2) {
+    asm volatile("mrs %0, s3_4_c1_c2_6" : "=r"(smcr));
+    smcr = (smcr & ~0xfUL) | 0xfUL;
+    asm volatile("msr s3_4_c1_c2_6, %0\n\tisb" : : "r"(smcr) : "memory");
+  }
+}
+#endif
+
 extern "C" [[gnu::weak]] void _platform_setup_arch_extensions() {
 #ifdef __ARM_FEATURE_PAUTH
   // Set all of the pointer authentication keys to different values. In
@@ -54,6 +83,7 @@ extern "C" [[gnu::weak]] void _platform_setup_arch_extensions() {
   // relevant feature these bits are ignored, so safe to set unconditionally.
   CPTR.EZ = 1;
   CPTR.ESM = 1;
+  init_sme_state();
 #else
   // Enable VFP and SIMD
   __arm_wsr("fpexc", 1 << 30);
---
+++ arm-software/embedded/llvmlibc-support/crt0/sme_abi.cpp
@@ -0,0 +1,37 @@
+//
+// Copyright (c) 2026, Arm Limited and affiliates.
+//
+// Part of the Arm Toolchain project, under the Apache License v2.0 with LLVM
+// Exceptions. See https://llvm.org/LICENSE.txt for license information.
+// SPDX-License-Identifier: Apache-2.0 WITH LLVM-exception
+//
+
+#if defined(__ARM_ARCH_ISA_A64)
+
+// Minimal SME ABI support for compiler-rt and LLVM libc users.
+// https://github.com/ARM-software/abi-aa/blob/main/aapcs64/aapcs64.rst#811__arm_sme_state
+asm(
+    ".text\n"
+    ".globl __arm_sme_state\n"
+    ".type __arm_sme_state, %function\n"
+    ".variant_pcs __arm_sme_state\n"
+    "__arm_sme_state:\n"
+    "  mov x0, xzr\n"
+    "  mov x1, xzr\n"
+    "\n"
+    "  /* Check whether SME or SME2 is present. */\n"
+    "  mrs x16, id_aa64pfr1_el1\n"
+    "  tst w16, #0x3000000\n"
+    "  b.eq .L__arm_sme_state_no_sme\n"
+    "\n"
+    "  orr x0, x0, #0xC000000000000000\n"
+    "  /* Read SVCR. */\n"
+    "  mrs x16, s3_3_c4_c2_2\n"
+    "  bfxil x0, x16, #0, #2\n"
+    "  /* Read TPIDR2_EL0. */\n"
+    "  mrs x1, s3_3_c13_c0_5\n"
+    ".L__arm_sme_state_no_sme:\n"
+    "  ret\n"
+    ".size __arm_sme_state, .-__arm_sme_state\n");
+
+#endif
