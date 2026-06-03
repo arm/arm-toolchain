741328f6476f03ee32e036838ff24895ea3d88f6
dcandler <david.candler@arm.com>
2026-06-03 09:44:44 +0100

    [ATfE] Build packages in subdirectory (#867)
    Instead of creating packages in the root of the build directory, put
    them in a subdirectory so they can be easily distinguished from the
    other build files.
    
    The scripts also now remove the CPack working directory once the package
    is built, so that the package directory should contain nothing but the
    package/packages.

--- arm-software/embedded/scripts/build_llvmlibc_toolchain.sh
+++ arm-software/embedded/scripts/build_llvmlibc_toolchain.sh
@@ -22,5 +22,13 @@ BUILD_DIR=${REPO_ROOT}/build_llvmlibc_toolchain
 mkdir -p "${BUILD_DIR}"
 cd "${BUILD_DIR}"
 
-cmake ../arm-software/embedded -GNinja -DFETCHCONTENT_QUIET=OFF -DLLVM_TOOLCHAIN_C_LIBRARY=llvmlibc
+cmake ../arm-software/embedded \
+    -GNinja \
+    -DFETCHCONTENT_QUIET=OFF \
+    -DCPACK_PACKAGE_DIRECTORY=atfe_packages \
+    -DLLVM_TOOLCHAIN_C_LIBRARY=llvmlibc
 ninja package-llvm-toolchain
+
+# Remove CPack working directory.
+rm -rf atfe_packages/_CPack_Packages
+
