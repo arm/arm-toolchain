7620fd3ab7e36ce5f43a74e951b871e5fb0fd81e
Vrukesh Panse <vrukesh.panse@arm.com>
2026-07-14 15:51:15 +0100

    [ATfL] Update Github Action flows for RHEL10 and Ubuntu 24.04
    Update Github Action workflows to build and test using Docker images for RHEL10 and Ubuntu 24.04.
    
    Add supported files needed for building Docker images.
    
    For both operating systems:
    * Build and test using Docker images.
    * Save XML test reports as artifacts.

---
+++ arm-software/linux/scripts/Docker_Files/install_entrypoint.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+set -e  # Exit immediately if a command exits with a non-zero status
+
+# Docker environment does not set the USER environment variable.
+# So rely on 'id'.
+CURRENT_USER_NAME="$(id -un)"
+
+# Source the script
+if [ -f "/home/${CURRENT_USER_NAME}/spack/share/spack/setup-env.sh" ]; then
+    source /home/${CURRENT_USER_NAME}/spack/share/spack/setup-env.sh
+fi
+
+# Execute the main command
+exec "$@"
