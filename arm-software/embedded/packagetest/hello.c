e126cec3fdd001df22e528d20eb6e3be82c29001
Volodymyr Turanskyy <volodymyr.turanskyy@arm.com>
2026-09-23 10:13:04 +0100

    [ATfE] Add missing cfg file in packagetests
    This adds the missing --config=<libc>.cfg command line option for the two hello.c/cpp package tests to make them work with LLVM libc.

--- arm-software/embedded/packagetest/hello.c
+++ arm-software/embedded/packagetest/hello.c
@@ -1,5 +1,5 @@
 // RUN: %clang -E -P -x c -DLIBC_LINKER_SCRIPT=%libc_linker_script %S/Inputs/microbit.ld -o %t.ld
-// RUN: %clang --target=armv6m-none-eabi -mfloat-abi=soft -march=armv6m -mfpu=none -nostartfiles -lcrt0-semihost -lsemihost -T %t.ld %s -o %t.out
+// RUN: %clang %libc_config --target=armv6m-none-eabi -mfloat-abi=soft -march=armv6m -mfpu=none -nostartfiles -lcrt0-semihost -lsemihost -T %t.ld %s -o %t.out
 // RUN: qemu-system-arm -M microbit -semihosting -nographic -device loader,file=%t.out 2>&1 | FileCheck %s
 
 #include <stdio.h>
